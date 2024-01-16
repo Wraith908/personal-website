dcc6ae6d071602998eeeb2abd2ac8016a0129340
Jacob Neilands <jn908@hotmail.com>
2024-01-16 20:41:18 +1100

    Bugfixing tailwindcss
    Tailwinds CSS wasn't automatically updating until I opened another commnnd line

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,22 +3,24 @@ import CardList from './CardList';
 
 export default function Main() {
     return(
-        <div className = "bg-sky-700">
-            <header>
-                <h1>App Name</h1>
-            </header>
-            <body>
-                <p>I need a <input list="CharTypes" placeholder = "Character"></input> <button>Go</button></p>
-                <datalist id = "CharTypes">
-                    <option value = "Character"/>
-                    <option value = "Villain"/>
-                    <option value = "Lackey"/>
-                    <option value = "Monster"/>
-                    <option value = "Hero"/>
-                    <option value = "Sidekick"/>
-                </datalist>
+        <div>
+            <span>
+                <h1>Dungeon Master Character Inspiration App</h1>
+            </span>
+            <div>
+                <form>
+                    <p>I need a <input list="CharTypes" placeholder = "Character" /> <button>Go</button></p>
+                    <datalist id = "CharTypes">
+                        <option value = "Character"/>
+                        <option value = "Villain"/>
+                        <option value = "Lackey"/>
+                        <option value = "Monster"/>
+                        <option value = "Hero"/>
+                        <option value = "Sidekick"/>
+                    </datalist>
+                </form>
                 <CardList />
-            </body>
+            </div>
         </div>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,8 @@ import type { Config } from 'tailwindcss'
 export default {
   content: [
     "./src/*.{html,tsx}",
-    "./src/components/*.{html,tsx}"
+    "./src/components/*.{html,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {},
