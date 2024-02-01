9ce9e0664efd0f95ed8af1f35d9efecc6e631ca1
rolandneilands <153884278+rolandneilands@users.noreply.github.com>
2024-02-01 14:43:11 +1100

    more grammar goodenising

--- src/components/FooterDetails.tsx
+++ src/components/FooterDetails.tsx
@@ -6,7 +6,7 @@ export default function FooterDetails() {
         <div>
             <p>Some information, such as class, race and patrons is copyright Wizards of the Coast and/or other authors. Such material is used with permission under the Community Content Agreement for Dungeon Masters Guild.</p>
             <p>All other original material in this <a href="https://github.com/Wraith908/personal-website">React/Typescript website and NPC generator</a> is copyright 2024 by <a href="https://www.linkedin.com/in/jacob-neilands-2748421b4/">Jacob Neilands</a> & <a href="https://www.linkedin.com/in/rolandneilands/">Roland Neilands</a> and published under the Community Content Agreement for Dungeon Masters Guild. </p>
-            <p>No private info is stored or cookies inflicted by this website. This site may contain references to death, difficult situations and embarrassing conditions for the purposes of dramatic role-playing inspiriation - use only at risk of your own offense.</p>
+            <p>No private info is stored or cookies inflicted by this website. This site may contain references to death, difficult situations and embarrassing conditions for the purposes of dramatic role-playing inspiriation - use your judgement to avoid risk of offending yourself and others.</p>
             <p>Any similarities in generated character names to real or imagined people is coincidental. Surnames are a list of historical job titles, and first names are randomly alternating vowel and non-vowel sounds.</p>
         </div>
     )
