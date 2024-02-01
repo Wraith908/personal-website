dba139e3edefda3e455c3e6be746acc71d673c32
rolandneilands <153884278+rolandneilands@users.noreply.github.com>
2024-02-01 11:04:39 +1100

    fix footer link and wording

--- src/components/FooterDetails.tsx
+++ src/components/FooterDetails.tsx
@@ -4,9 +4,9 @@ export default function FooterDetails() {
 
     return(
         <div className="footer text-center">
-            <p>Some information, such as class, race and patrons is copyright Wizards of the Coast and/or other authors. Such material is used with permission under the Community Content Agreement for Dungeon Masters Guild.</p>
-            <p>All other original material in this <a href="https://github.com/Wraith908/personal-website">React/Typescript website and NPC generator</a> is copyright 2024 by <a href="https://www.linkedin.com/in/jacob-neilands-2748421b4/">Jacob Neilands</a> & <a href="https://www.linkedin.com/in/rolandneilands/">Roland Neilands and published under the Community Content Agreement for Dungeon Masters Guild.</a> </p>
-            <p>No privacy or cookies were harmed in the making of this website</p>
+            <p>Some material, such as class, race and patrons is copyright Wizards of the Coast and/or other authors, and is referenced with page numbers where possible and used with permission under the Community Content Agreement for Dungeon Masters Guild.</p>
+            <p>All other original material in this <a href="https://github.com/Wraith908/personal-website">D&D 5E NPC generator website built with React & Typescript</a> is copyright 2024 by <a href="https://www.linkedin.com/in/jacob-neilands-2748421b4/">Jacob Neilands</a> & <a href="https://www.linkedin.com/in/rolandneilands/">Roland Neilands</a> and published under the Community Content Agreement for Dungeon Masters Guild. </p>
+            <p>No private info is collected or cookies inflicted by this website</p>
             <p>Any similarities in generated character names to real or imagined people is coincidental. Surnames are a list of historical job titles, and first names are randomly alternating vowel and non-vowel sounds.</p>
         </div>
     )
